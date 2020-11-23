1c950fbd3a5d7f3537c81b7fed7be5c061ce7a1f
Oskar Pawica <pawicao@student.agh.edu.pl>
2020-11-23 13:46:13 +0000

    Updated add-permissions.sh

--- scripts/add-permissions.sh
+++ scripts/add-permissions.sh
@@ -1,2 +1,3 @@
 sed -i '/<uses-permission*/ a <uses-permission android:name="android.permission.CAMERA" /><uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />' ./android/app/src/main/AndroidManifest.xml
 sed -i '/studying_squirrels_ui/c\<string name="app_name">Studying Squirrels</string>' ./android/app/src/main/res/values/strings.xml
+cp -R ./src/assets/icons/res ./android/app/src/main
